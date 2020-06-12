d4aeef606491cead6d8fb4427a441f0addaa9a6c
diegoco20 <diego.co202@gmail.com>
2020-06-12 17:56:34 -0500

    commit

---
+++ Plot1.R
@@ -0,0 +1,22 @@
+directory_1=getwd()
+#Loading Libraries
+
+library(dplyr)
+
+#Loading Data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI_TBL=tbl_df(NEI)
+#Grouping Data
+NEI_TBL=group_by(NEI_TBL, year)
+rm(NEI)
+
+#Summarizing Data
+mean_year=summarize(NEI_TBL, mean(Emissions))
+mean_year=as.data.frame(mean_year)                    
+names(mean_year)=c("Year", "Emissions")
+
+#Creating Plot
+png(file = "plot1.png", width = 480, height = 480)
+with(mean_year, barplot(Emissions, names.arg = Year, xlab="Year", ylab="PM25-PRI Emissions", main="PM 25 Emissions by Year"))
+dev.off()
